37c08b6198684925714a2e2c69c71e41263aefde
Kendall Cole <kendall@veenome.com>
2014-08-23 17:09:21 -0400

    Fix patch request handling

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ import psycopg2
 import sys
 
 def get_database_connection():
+	"""Connect to postgres db using psycopg2"""
 	conn_string = "dbname='unbabel'"
 	conn = psycopg2.connect(conn_string)
 	cursor = conn.cursor()
