77c57b14e2b0046e0f94ac1be03a495f580e6d2e
Kendall Cole <kendall@veenome.com>
2014-08-23 16:11:43 -0400

    Clean up db.py

--- db.py
+++ db.py
@@ -1,7 +1,5 @@
 import psycopg2
 import sys
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
 
 def get_database_connection():
 	conn_string = "dbname='unbabel'"
